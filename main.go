2c45d2c9bc039b1ff19d4d1d238dd40d815b58c1
sparkidea25 <michaeldaralola123@gmail.com>
2020-12-29 09:54:52 +0100

    done

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -42,8 +43,10 @@ func CreateDew(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 
+	PORT := os.Getenv("PORT")
+
 	router.HandleFunc("/dews", GetDew).Methods("GET")
 	router.HandleFunc("/dew", CreateDew).Methods("POST")
 	log.Println("Listening on Port: 7070")
-	log.Fatal(http.ListenAndServe(":7070", router))
+	http.ListenAndServe(":"+PORT, router)
 }
